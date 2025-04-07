995fee01295834a98daa72ce9beaeb1cbb1d03dc
AMOLIXS25 <wilmet.amaury@protonmail.com>
2025-04-07 15:38:18 +0200

    05_sumAll exercise finish

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(rangeFirstNumber, rangeSecondNumber) {
+    if (rangeFirstNumber < 0 || rangeSecondNumber < 0) return 'ERROR';
+    if (!Number.isInteger(rangeFirstNumber) || !Number.isInteger(rangeSecondNumber)) return 'ERROR';
+    let result = 0;
+    for (let i = ((rangeFirstNumber < rangeSecondNumber) ? 
+            rangeFirstNumber : rangeSecondNumber); 
+            i <= ((rangeFirstNumber < rangeSecondNumber) ? 
+            rangeSecondNumber : rangeFirstNumber); i++
+        ) {
+            result += i;
+    }
+    return result;
+}; 
 
-};
+console.log(sumAll(2, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
