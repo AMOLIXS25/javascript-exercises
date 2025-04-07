b9b813e138e02a5ce15d7ecfe2c7a9c81900cfcd
AMOLIXS25 <wilmet.amaury@protonmail.com>
2025-04-07 16:00:44 +0200

    07_tempConversion finish

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  return parseFloat((5/9*(fahrenheitTemp-32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  return parseFloat(((celsiusTemp * 9/5) + 32).toFixed(1))
 };
 
 // Do not edit below this line
