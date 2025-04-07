7e0e975a64d2a3cfe719bd0d43888b27f7275bcb
AMOLIXS25 <wilmet.amaury@protonmail.com>
2025-04-07 18:00:32 +0200

    10_fibonacci exercise finish

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = (numberOfFibonacciSuite) => {
+    if (numberOfFibonacciSuite < 0) return "OOPS"; 
+    let arrayOfFibonacciNumbers = [0, 1, 1];
+    numberOfFibonacciSuite = parseInt(numberOfFibonacciSuite);
 
+    for (let i = 1; i < numberOfFibonacciSuite - 1; i++) {
+        let resultTwoPrecedentNumbers = 0;
+        for (let j = arrayOfFibonacciNumbers.length - 1; j > arrayOfFibonacciNumbers.length - 3; j--) {
+            resultTwoPrecedentNumbers += arrayOfFibonacciNumbers[j];
+        }
+        arrayOfFibonacciNumbers.push(resultTwoPrecedentNumbers);
+    }
+    return arrayOfFibonacciNumbers[numberOfFibonacciSuite];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
