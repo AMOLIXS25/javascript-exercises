7102939b4f1f6f44f6d71e9e65fdd745db31e62b
AMOLIXS25 <wilmet.amaury@protonmail.com>
2025-04-07 15:48:59 +0200

    06_leapYears exercise finish

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(yearToVerifIfLeap) {
+    if (yearToVerifIfLeap % 100 === 0 && yearToVerifIfLeap % 400 !== 0) {
+        return false;
+    }else if (yearToVerifIfLeap % 100 === 0 && yearToVerifIfLeap % 400 === 0) {
+        return true;
+    } else if (yearToVerifIfLeap % 4 === 0) {
+        return true;
+    }
 
+    return false;
 };
 
 // Do not edit below this line
