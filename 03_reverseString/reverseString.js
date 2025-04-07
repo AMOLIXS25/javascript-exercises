057e8a665cbdfd809921e24a81e1dcb1d28fecac
AMOLIXS25 <wilmet.amaury@protonmail.com>
2025-04-07 12:35:03 +0200

    Finish 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(chaineToReverse) {
+    return chaineToReverse.split('').reverse().join('')
 };
 
 // Do not edit below this line
