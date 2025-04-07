ab8835dbf38c7fdca629b7aeb550123cace46995
AMOLIXS25 <wilmet.amaury@protonmail.com>
2025-04-07 12:16:20 +0200

    Finish 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(chaineToRepeat, numberOfRepetition) {
+    let resultStr = ((numberOfRepetition >= 0) ? '' : 'ERROR');
+    for (let i = 0; i < numberOfRepetition; i++) {
+        resultStr += chaineToRepeat;
+    }
+    return resultStr;
 };
 
 // Do not edit below this line
