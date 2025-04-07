c251528fb38d0cd321dc4bb07a75c81e93d63cb9
AMOLIXS25 <wilmet.amaury@protonmail.com>
2025-04-07 16:53:27 +0200

    09_palindromes exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = (strToVerifIfPalindrome) => {
+    let newStrToVerifWithoutSpaceAndPunctuation = strToVerifIfPalindrome;
+    [',', '!', '.', '?', ' '].forEach((punctuation) => {
+        newStrToVerifWithoutSpaceAndPunctuation = newStrToVerifWithoutSpaceAndPunctuation.replaceAll(punctuation, '') 
+    });
+    newStrToVerifWithoutSpaceAndPunctuation = newStrToVerifWithoutSpaceAndPunctuation.toLowerCase();
+    return newStrToVerifWithoutSpaceAndPunctuation === newStrToVerifWithoutSpaceAndPunctuation.split('').reverse().join('');
 };
 
 // Do not edit below this line
