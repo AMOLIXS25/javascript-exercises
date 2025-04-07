d8362aa8a0ab6f939f49ae4a6aeac00f12ac748a
AMOLIXS25 <wilmet.amaury@protonmail.com>
2025-04-07 13:05:38 +0200

    04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(baseArray, ...elementsToRemove) {
+    elementsToRemove.forEach((elementToRemove) => {
+        let numberOfOccurence = 0;
+        baseArray.forEach((element) => {
+            if (elementToRemove == element) {
+                numberOfOccurence++;
+            }
+        })
+        baseArray.splice(baseArray.indexOf(elementToRemove), numberOfOccurence);
+    })
+    return baseArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 1, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
